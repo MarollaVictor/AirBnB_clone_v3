50bdb199d81c8c19f6258e4e05fa996ec04c718c
MarollaVictor <marollavictorsedky@gmail.com>
2025-02-18 18:39:32 +0200

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module for the stats endpoint of the API
+"""
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
-@app_views.route('/status', methods=['GET'])
-def status():
-    """Returns API status"""
+@app_views.route('/status', strict_slashes=False)
+def get_status():
+    """Return the status of the API"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def get_stats():
+    """Retrieve the stats of all objects"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
