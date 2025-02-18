c0fd667f8964defc761dbb3982897cd0f4bc25c3
FadySabry712 <fadysabry95@yahoo.com>
2025-02-18 14:51:26 -0800

    fixed formatting

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,11 +21,16 @@ def teardown_db(exception):
 
 @app.errorhandler(404)
 def not_found(error):
-    """Handler for 404 errors"""
+    """Handler for 404 errors
+    resp:
+    404: resource not found
+
+    """
     return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
+    """ Main Application Function """
     host = environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True, debug=True)
